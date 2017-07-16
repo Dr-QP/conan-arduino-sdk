69744d836d4d5d3708536f06f20ec267b74e538a
Anton Matosov <anton.matosov@gmail.com>
2017-07-15 22:29:29 -0700

    Fix options definition

--- build.py
+++ build.py
@@ -4,11 +4,11 @@ from conans.tools import os_info
 if __name__ == "__main__":
     builder = ConanMultiPackager(args="--build missing", username="anton-matosov", channel="stable")
     builder.add(options={
-        "use_bundled_java=False"
+        "conan-ardiono-sdk:use_bundled_java": False
     })
     if os_info.is_linux or os_info.is_windows:
         builder.add(options={
-            "use_bundled_java=True"
+            "conan-ardiono-sdk:use_bundled_java": True
         })
 
     builder.run()
