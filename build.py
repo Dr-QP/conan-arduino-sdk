db4f388e88ad9530a76e858691e81afb9d459ce6
Anton Matosov <anton.matosov@gmail.com>
2017-07-15 17:46:28 -0700

    Setup proper builds

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="anton-matosov", channel="stable")
-    
+    builder = ConanMultiPackager(args="--build missing", username="anton-matosov", channel="stable")
+    builder.add()
+
     builder.run()
