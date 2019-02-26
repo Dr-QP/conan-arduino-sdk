3513ec4412f216938ec984e0963c3ccd17910e33
Anton Matosov <anton.matosov@gmail.com>
2019-02-25 21:10:30 -0800

    try other syntax

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,7 +7,7 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update
     brew upgrade python3
 elif [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
-    cinst -y python3
+    choco install -y python3
 fi
 
 pip3 install conan conan_package_tools --upgrade
