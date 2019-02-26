e0b4d797870be4d54554f8d56031f2a579099f34
Anton Matosov <anton.matosov@gmail.com>
2019-02-25 21:16:41 -0800

    one more try to get chocolatey work

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,7 +7,12 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update
     brew upgrade python3
 elif [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
-    choco install -y python3
+    choco install python3 --params "/InstallDir:/c/python3"
+fi
+
+if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+    export PATH="$PATH:/c/python3/bin"
+    tree /c/python3
 fi
 
 pip3 install conan conan_package_tools --upgrade
