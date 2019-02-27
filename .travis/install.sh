a910cbf0b4c03bbc4f2cc5410322218827d2b9fb
Anton Matosov <anton.matosov@gmail.com>
2019-02-26 19:38:26 -0800

    travis for windows doesn't work

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,13 +6,6 @@ set -x
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update
     brew upgrade python3
-elif [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
-    choco install python3 --params "/InstallDir:/c/python3"
-fi
-
-if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
-    export PATH="$PATH:/c/python3/bin"
-    tree /c/python3
 fi
 
 pip3 install conan conan_package_tools --upgrade
