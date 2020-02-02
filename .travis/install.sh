b4b14d5d4edc307040d8917bf7e6441caf663a15
Anton Matosov <anton.matosov@gmail.com>
2020-02-01 22:55:13 -0800

    comment out entire if

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,10 +3,10 @@
 set -e
 set -x
 
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-    # brew update
-    # brew upgrade python3
-fi
+# if [[ "$(uname -s)" == 'Darwin' ]]; then
+#     brew update
+#     brew upgrade python3
+# fi
 
 pip3 install conan conan_package_tools --upgrade
 
