79d4d04d37ed772e8cf929ea553f103a0ac48a03
Anton Matosov <anton.matosov@gmail.com>
2020-02-01 22:51:44 -0800

    don't touch python

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,8 +4,8 @@ set -e
 set -x
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
-    brew update
-    brew upgrade python3
+    # brew update
+    # brew upgrade python3
 fi
 
 pip3 install conan conan_package_tools --upgrade
