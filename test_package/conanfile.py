935d61e14f0d6ea75a45af016cf1b4c1f05b7585
Anton Matosov <anton.matosov@gmail.com>
2020-02-09 11:06:06 -0800

    Add real compilation test to SDK (#3)
    * fix line endings mismatch for windows and mac
    
    * add real cpp compilation test

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -3,14 +3,20 @@ import os
 
 class ConanArduinoSDKTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def test(self):
-        arduino_path = os.getenv("CONAN_ARDUINO_SDK_PATH")
+        arduino_path = os.getenv("ARDUINO_SDK_PATH")
         if not arduino_path:
             raise Exception(
-                "CONAN_ARDUINO_SDK_PATH environment variable is not set")
+                "ARDUINO_SDK_PATH environment variable is not set")
         if not os.path.exists(arduino_path):
             raise Exception(
-                "CONAN_ARDUINO_SDK_PATH folder doesn't exist: %s" % arduino_path)
-                
+                "ARDUINO_SDK_PATH folder doesn't exist: %s" % arduino_path)
+
         self.output.success("Done")
